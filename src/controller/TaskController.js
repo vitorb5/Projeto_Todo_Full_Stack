5cdfecdf078d9e32a9150e9e461654c78f546a50
Vitor R. Bizzi <vitorbizzi5@gmail.com>
2020-09-06 08:48:37 -0300

    Criação do crud, delete, done e late

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,5 +1,8 @@
 const TaskModel = require ('../model/TaskModel');
 const { response } = require('express');
+const { restart } = require('nodemon');
+const current = new Date();
+
 class TaskController{
 	  async create (req, res){
 		const task = new TaskModel(req.body);
@@ -36,5 +39,50 @@ class TaskController{
 	}
 	//$in ou equals para buscar valores que existam relacioanado aquela busca especifica
 	//short para trazer organizado por aquele parametro
+
+	async show(req, res){
+		await TaskModel.findById(req.params.id)
+		.then(response => {
+				if(response)
+			return res.status(200).json(response);
+				else
+			return res.status(404).json({error : 'Não existe tarefa com esse id'})
+		})
+		.catch(error => {
+			return res.status(500).json(error);
+		})
+	}
+
+	async delete (req, res){
+		await TaskModel.deleteOne({'_id' : req.params.id})
+		.then(response =>{
+				return res.status(200).json(response);
+		})
+		.catch(error =>{
+			return res.status(500).json(error);
+		});
+	}	
+
+	async done(req, res){
+		await TaskModel.findByIdAndUpdate({'_id' : req.params.id}, {'done' : req.params.done}, {new : true})
+		.then(response => {
+			return res.status(200).json(response);
+		})
+		.catch(error =>{
+			return res.status(500).json(error);
+		});
+	}
+	async late(req, res){
+		await TaskModel.find({'when' : {'$lt' : current}, 'macaddress' : {$in : req.body.macaddress }})
+		//$lt é igual ou menor serve para fazer comparação, como no exemplo usado que preciso verificar as 
+		//datas das tarefas atrasadas até o dia e hora dop memento da pesquisa.
+		.sort('when')
+		.then(response =>{
+			return res.status(200).json(response);
+		})
+		.catch(error =>{
+			return res.status(500).json(error);
+		});
+	}
 }
 module.exports = new TaskController();
--- src/routes/TaskRoutes.js
+++ src/routes/TaskRoutes.js
@@ -7,6 +7,10 @@ const MacaddressValidation = require('../middlewares/MacaddressValidation');
 
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
+router.get('/:id', TaskController.show);
+router.delete('/:id', TaskController.delete);
+router.put('/:id/:done', TaskController.done);
 router.get('/filter/all', MacaddressValidation,  TaskController.all);
+router.get('/filter/late', MacaddressValidation, TaskController.late),
 
 module.exports = router;
